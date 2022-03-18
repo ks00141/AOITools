e05090614a29cb08bb36ce01650a3f1373b1faa8
oruparu <ks00141@naver.com>
2022-03-18 16:25:07 +0900

    teams 채팅 수집기

--- opencv/main.py
+++ opencv/main.py
@@ -5,14 +5,18 @@ import time
 from PIL import Image
 
 
-p = re.compile('WG42AGA')
-root_path = r'C:\Users\wisol\Desktop\python_dev\AOITools\opencv\WG42AGA'
+p = re.compile('[C|F][_]')
+root_path = r'C:\Users\wisol\Desktop\python_dev\AOITools\opencv\03161'
 
 file_list = os.listdir(root_path)
 
-file_list = [file for file in file_list if p.match(file)]
+file_list = [file for file in file_list if not p.match(file)]
+
+file_list = [file for file in file_list if os.path.isfile(os.path.join(root_path, file))]
+
 start = time.time()
 for file in file_list:
+    print(file)
     img = Image.open(os.path.join(root_path, file))
     np_img = np.array(img)
     np_img_1d = np_img.mean(axis=2)
